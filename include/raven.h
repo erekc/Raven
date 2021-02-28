57ab9c275fdaccb4500c5c7c8b1bceeb2e893028
erekc <erek.r.cox@gmail.com>
2021-02-28 08:56:27 -0500

    added raven parent class

---
+++ include/raven.h
@@ -0,0 +1,13 @@
+#ifndef RAVEN_RAVEN_H
+#define RAVEN_RAVEN_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Raven {
+public:
+    virtual void solve() = 0;
+};
+
+#endif //RAVEN_RAVEN_H
