48f9a51d1ddb15b9d1cf79819b462beee389342c
erekc <erek.r.cox@gmail.com>
2021-03-05 19:54:52 -0500

    added read_input function & input_file members

--- include/raven.h
+++ include/raven.h
@@ -4,10 +4,17 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
+
+using namespace std;
 
 class Raven {
 public:
     virtual void solve() = 0;
+    virtual void read_input() = 0;
+
+protected:
+    ofstream input_file;
 };
 
 #endif //RAVEN_RAVEN_H
