e8f58bf762f851ce857e7887911ef838be32750a
erekc <erek.r.cox@gmail.com>
2021-03-06 08:48:07 -0500

    added test fixture for jewels and stones

---
+++ test/jewels_and_stones_test.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+#include "../include/jewels_and_stones.h"
+
+class JewelsAndStonesTest : public ::testing::Test {
+protected:
+    JewelsAndStones jewels_and_stones = JewelsAndStones();
+};
+
