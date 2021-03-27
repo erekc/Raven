71cfa689ce513ffcc7c5953403cabe12ccfc2080
erekc <erek.r.cox@gmail.com>
2021-03-27 08:06:42 -0400

    added main & test case for hello

--- src/main/hello_main.cpp
+++ src/main/hello_main.cpp
@@ -1,2 +1,8 @@
 #include "../../include/hello.h"
 
+int main() {
+    Hello hello;
+    hello.read_input("../input/hello.txt");
+    hello.solve();
+    return 0;
+}
--- test/hello_test.cpp
+++ test/hello_test.cpp
@@ -5,3 +5,9 @@ class HelloTest: public ::testing::Test {
 protected:
     Hello hello;
 };
+
+TEST_F(HelloTest, hello_world) {
+    hello.set_input("Hello World!");
+    hello.solve();
+    EXPECT_TRUE(hello.get_output() == "Hello World!");
+}
