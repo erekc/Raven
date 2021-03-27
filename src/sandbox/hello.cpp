793116f063228f7118d03b93fcba080c6df62ecf
erekc <erek.r.cox@gmail.com>
2021-03-27 07:56:20 -0400

    implemented hello methods

--- src/sandbox/hello.cpp
+++ src/sandbox/hello.cpp
@@ -1,4 +1,23 @@
 #include "../../include/hello.h"
 
+void Hello::read_input(std::string filename) {
+    this->input_file.open(filename);
+    if (this->input_file.is_open()) {
+        std::getline(this->input_file, this->input);
+        this->input_file.close();
+    }
+    else {
+        std::cout << "Unable to open file" << std::endl;
+    }
+}
 
+std::string Hello::hello(std::string hello) {
+    return hello;
+}
+
+void Hello::solve() {
+    this->output = this->hello(this->input);
+    std::cout << this->output << std::endl;
+    std::cout << "Welcome to Raven! An interactive sandbox for solving algo puzzles!" << std::endl;
+}
 
